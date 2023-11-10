7abfe5bc1ac4e6def6a736160907c66f6c97607b
Gilberto Pereira Sassi <sassi.pereira.gilberto@gmail.com>
2023-11-10 17:17:51 +0000

    moda e quantil para binomial e poisson

--- probabilidade-1/banco-itens/moda-quantil-va-discreta/exportacao.R
+++ probabilidade-1/banco-itens/moda-quantil-va-discreta/exportacao.R
@@ -16,4 +16,4 @@ exams2moodle(glue("{path}/enunciado.Rmd"),
                     n = 1000,
                     verbose = TRUE,
                     dir = glue("{path}"),
-                    name = "moda-quantil-va-discreta-teste")
+                    name = "moda-quantil-va-discreta")
