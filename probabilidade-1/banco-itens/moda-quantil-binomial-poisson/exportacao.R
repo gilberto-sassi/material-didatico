c1519b32a7559493c65b8b46f64c8415d23520ea
Gilberto Pereira Sassi <sassi.pereira.gilberto@gmail.com>
2023-11-10 16:51:27 +0000

    moda e quantil para binomial e poisson

--- probabilidade-1/banco-itens/moda-quantil-binomial-poisson/exportacao.R
+++ probabilidade-1/banco-itens/moda-quantil-binomial-poisson/exportacao.R
@@ -2,18 +2,17 @@ pacman::p_load(
     glue, exams
 )
 
-path <- "probabilidade-1/banco-itens/moda-quantil-va-discreta"
-
+path <- "probabilidade-1/banco-itens/moda-quantil-binomial-poisson"
 # teste
 exams2moodle(glue("{path}/enunciado.Rmd"),
                     n = 5,
                     verbose = TRUE,
                     dir = glue("{path}"),
-                    name = "moda-quantil-va-discreta-teste")
+                    name = "moda-quantil-va-binomial-poisson-teste")
 
 # real
 exams2moodle(glue("{path}/enunciado.Rmd"),
                     n = 1000,
                     verbose = TRUE,
                     dir = glue("{path}"),
-                    name = "moda-quantil-va-discreta-teste")
+                    name = "moda-quantil-va-binomial-poisson")
--- probabilidade-1/banco-itens/moda-quantil-va-continua/exportacao.R
+++ probabilidade-1/banco-itens/moda-quantil-va-continua/exportacao.R
@@ -3,8 +3,16 @@ pacman::p_load(
 )
 
 path <- "probabilidade-1/banco-itens/moda-quantil-va-continua"
+# teste
 exams2moodle(glue("{path}/enunciado.Rmd"),
                     n = 5,
                     verbose = TRUE,
                     dir = glue("{path}"),
                     name = "moda-quantil-va-continua-teste")
+
+# real
+exams2moodle(glue("{path}/enunciado.Rmd"),
+                    n = 1000,
+                    verbose = TRUE,
+                    dir = glue("{path}"),
+                    name = "moda-quantil-va-continua")
