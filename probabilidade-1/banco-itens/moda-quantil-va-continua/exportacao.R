7c50833579b9da67d79a5d1e89d28aea17d1f5b3
Gilberto Pereira Sassi <sassi.pereira.gilberto@gmail.com>
2023-11-10 16:08:05 +0000

    moda quantil para va continua

--- probabilidade-1/banco-itens/moda-quantil-va-continua/exportacao.R
+++ probabilidade-1/banco-itens/moda-quantil-va-continua/exportacao.R
@@ -7,4 +7,4 @@ exams2moodle(glue("{path}/enunciado.Rmd"),
                     n = 5,
                     verbose = TRUE,
                     dir = glue("{path}"),
-                    name = "moda-quantil-va-continua")
+                    name = "moda-quantil-va-continua-teste")
