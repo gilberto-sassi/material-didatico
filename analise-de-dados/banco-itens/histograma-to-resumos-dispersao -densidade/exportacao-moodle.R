4dd0ac8734903a421309fba95ff94612443da74a
Gilberto Pereira Sassi <sassi.pereira.gilberto@gmail.com>
2024-11-22 09:27:41 -0300

    nova pergunta: histograma para medidas de resumo usando densidade

---
+++ analise-de-dados/banco-itens/histograma-to-resumos-dispersao -densidade/exportacao-moodle.R
@@ -0,0 +1,26 @@
+library(pacman)
+p_load(readxl)
+p_load(readODS)
+p_load(writexl)
+p_load(ggthemes)
+p_load(glue)
+p_load(statBasics)
+p_load(exams)
+p_load(tidyverse)
+
+path <- "analise-de-dados/banco-itens/histograma-to-resumos-dispersao -densidade"
+
+# 5
+exams2moodle(glue("{path}/enunciado.Rmd"),
+                    n = 5,
+                    verbose = TRUE,
+                    dir = glue("{path}"),
+                    name = "histograma-to-resumos-dispersao-densidade-teste")
+
+
+# 250
+exams2moodle(glue("{path}/enunciado.Rmd"),
+                    n = 250,
+                    verbose = TRUE,
+                    dir = glue("{path}"),
+                    name = "histograma-to-resumos-dispersao-densidade")
