5d874e3d553d8485325ce663d1131b5781262e18
Gilberto Pereira Sassi <sassi.pereira.gilberto@gmail.com>
2025-04-25 12:33:32 -0300

    atualizei a pergunta de probabilidade frequentista

--- probabilidade-1/banco-itens/prob_frequentista/exportacao.R
+++ probabilidade-1/banco-itens/prob_frequentista/exportacao.R
@@ -7,7 +7,15 @@ library(statBasics)
 library(exams)
 library(tidyverse)
 
-path <- "banco-itens/prob_frequentista"
+path <- "probabilidade-1/banco-itens/prob_frequentista"
+
+exams::exams2moodle(glue("{path}/enunciado.Rmd"),
+                    n = 5,
+                    verbose = TRUE,
+                    dir = glue("{path}"),
+                    name = "prob_frequentista_teste")
+
+
 exams::exams2moodle(glue("{path}/enunciado.Rmd"),
                     n = 1000,
                     verbose = TRUE,
